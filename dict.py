173730e32de069d8c692cd6b44c99020b9b1fdf8
maristie <heyanmailbox@gmail.com>
2017-12-04 02:19:56 +0800

    Fix naming

--- dict.py
+++ dict.py
@@ -7,7 +7,7 @@ def get_gram(line, index):
 
     # Get 1, 2, 3-grams with position information
     # p for previous, m for middle, n for next
-    gram_set.add(line[index] + '_m')
+    gram_set.add(line[i] + '_m')
     if i == 0 and i + 1 < length:
         gram_set |= {line[i + 1] + '_n', line[i:i + 2] + '_mn'}
     elif i == length - 1:
