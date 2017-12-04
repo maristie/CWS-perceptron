5cf97c0494d33f6744f8b10bf521a9a9dc2406a6
maristie <heyanmailbox@gmail.com>
2017-12-04 17:46:58 +0800

    Remove union set operation

--- dict.py
+++ dict.py
@@ -2,18 +2,17 @@ from parser import parse
 
 def get_gram(line, index):
     length = len(line)
-    gram_set = set()
-    i = index + 1
+    i = index + 1   # As we've added a header ^
 
     # ^ for begin, $ for end
     temp_line = '^' + line + '$'
 
     # Get unigrams and bigrams with position information
     # p for previous, m for middle, n for next
-    gram_set |= {temp_line[i] + '_m', temp_line[i + 1] + '_n',
-                 temp_line[i - 1] + '_p', temp_line[i:i + 2] + '_mn', temp_line[i - 1:i + 1] + '_pm',
-                 temp_line[i - 1] + temp_line[i + 1] + '_pn',
-                 temp_line[i - 1:i + 2]}
+    gram_set = {temp_line[i] + '_m', temp_line[i + 1] + '_n',
+                temp_line[i - 1] + '_p', temp_line[i:i + 2] + '_mn', temp_line[i - 1:i + 1] + '_pm',
+                temp_line[i - 1] + temp_line[i + 1] + '_pn',
+                temp_line[i - 1:i + 2]}
 
     return gram_set
 
